81be90a56c72e08cb74f5a0a178eef81489a4006
datbuiquoc035 <datbuiquoc035@github.com>
2026-07-26 14:31:45 +0700

    Fix bootstrap: use & instead of Start-Process to keep console

--- winsetup.ps1
+++ winsetup.ps1
@@ -6,7 +6,9 @@ Write-Host "Downloading WinSetup..." -ForegroundColor Cyan
 Invoke-WebRequest -Uri "$repo/Winsetup.App.exe" -OutFile "$dir\Winsetup.App.exe"
 Invoke-WebRequest -Uri "$repo/winsetup-core.exe" -OutFile "$dir\winsetup-core.exe"
 
+Push-Location $dir
 Write-Host "Launching WinSetup..." -ForegroundColor Cyan
-Start-Process -FilePath "$dir\Winsetup.App.exe" -Wait -WindowStyle Normal
+& ".\Winsetup.App.exe"
+Pop-Location
 
 Remove-Item -Recurse -Force $dir
