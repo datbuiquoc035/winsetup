53eeaff4f0d62aefb2b39b077b32741587e22445
datbuiquoc035 <datbuiquoc035@github.com>
2026-07-26 12:26:17 +0700

    Minimal XAML to isolate compiler issue

--- src/Winsetup.App/MainWindow.xaml.cs
+++ src/Winsetup.App/MainWindow.xaml.cs
@@ -1,50 +1,15 @@
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Winsetup.App.ViewModels;
 
 namespace Winsetup.App;
 
 public partial class MainWindow : Window
 {
-    private readonly MainViewModel _vm;
-
     public MainWindow()
     {
         InitializeComponent();
-        _vm = new MainViewModel();
-        AppList.ItemsSource = _vm.Items;
-        InstallButton.IsEnabled = false;
-
-        _vm.PropertyChanged += (_, e) =>
-        {
-            if (e.PropertyName == nameof(MainViewModel.IsInstalling))
-            {
-                InstallButton.IsEnabled = !_vm.IsInstalling;
-                InstallProgress.Visibility = _vm.IsInstalling
-                    ? Visibility.Visible
-                    : Visibility.Collapsed;
-                InstallButton.Content = _vm.IsInstalling ? "Installing..." : "Install Selected";
-            }
-        };
     }
 
-    private async void OnInstallClicked(object sender, RoutedEventArgs e)
+    private void OnInstallClicked(object sender, RoutedEventArgs e)
     {
-        InstallProgress.IsIndeterminate = true;
-        await _vm.InstallSelectedAsync();
-        InstallProgress.IsIndeterminate = false;
-    }
-
-    private void OnSearchSubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
-    {
-        _vm.Filter(args.QueryText);
-    }
-
-    private void OnSearchChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-    {
-        if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
-        {
-            _vm.Filter(sender.Text);
-        }
     }
 }
